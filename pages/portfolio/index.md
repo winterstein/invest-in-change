58414c37009af48d648f041bd083e16931089f4b
aundiks@gmail.com <andris@good-loop.com>
2019-09-25 12:20:33 +0100

    Script, styles and MailChimp scraper... but no html.

--- pages/portfolio/index.md
+++ pages/portfolio/index.md
@@ -15,17 +15,8 @@ Dec https://mailchi.mp/ace59fd0fbd4/invest-in-change-december-opportunities
 
 TODO
 
-<section src='template-company.html'>
-name: Deciwatt
-tagline: Instant and accessible renewable energy products
-image: https://gallery.mailchimp.com/08707f64623756d8a0ef4c72d/images/10617a0e-46d2-4f50-b2c7-f091e9d94a31.jpg
-location: London, UK
-tax-relief: EIS eligible
-impact-type: Affordable and Clean Energy (UN Goal #7)
-description: "Deciwatt develop renewable energy solutions and the newest product, NowLight creates instant light and power from the pull of a cord. Just one minute of pulling on NowLight’s cord will generate up to two hours of light and when fully charged NowLight will provide up to 50 hours of light, as well as charge mobile devices. It can be used recreationally and has particularly interesting potential in humanitarian aid contexts or for the billions of people worldwide who lack reliable sources of energy or light."
-campaign-sitename: Seedrs
-campaign-link: https://www.seedrs.com/deciwatt/sections/idea
-website: http://deciwatt.global/
+<section src='index.html'>
+
 </section>
 
 
--- webroot/js/portfolio/portfolio.js
+++ webroot/js/portfolio/portfolio.js
@@ -2,7 +2,7 @@ import { companies, states } from './companies.js';
 
 const $cardContainer = $('.card-container');
 const $companyCard = $('.company-card');
-const $sortRegionBtn = $('.sort-region-btn');
+const $navRegionBtn = $('.nav-region');
 
 let currentRegion = 'all';
 
@@ -10,26 +10,51 @@ const generateCardLinks = links => {
     let renderedLinks = '';
     links.forEach( link => {
         const linkKey = Object.keys(link)[0];
-        renderedLinks += `<a href="${link[linkKey]}">${linkKey}</a>`
+        renderedLinks += `<a class="company-link" href="${link[linkKey]}">${linkKey}</a>`
     });
     return renderedLinks;
 };
 
+const formatCompanyName = name => {
+    const separateName = name.split(': ');
+    return `<h3><span class="red-text">${separateName[0]}</span></h3>
+            <h4><span> ${separateName[1]}</span></h4>`;
+};
+
+const formatInvestType = (type, link) => {
+    const splitString = type.split('(');
+    return link ? `<p>${splitString[0]}<a href="${link}">(${splitString[1]}</a></p>`
+                : `<p>${type}</p>`;
+};
+
+const itemHighlighter = item => {
+    const itemArray = item.split(':');
+    return `<p><span style="font-weight: bold">${itemArray[0]}</span>: ${itemArray[1]}</p>`
+};
+
+// Generate html card 'component' and return it, as long as it belong to currentRegion
 const generateCompanyCard = card => {
     const linkElements = generateCardLinks(card.links);
-    const cardDiv = `<div class="company-card row">
-                    <div class="col-sm-8">
-                       <h3 class="card-title">${card.name}</h3>
-                        <h5 class="card-subtitle">${card.location}</h5>
-                        <p class="card-text">Impact type: ${card.impactType}</p>
-                        <p class="card-text">Description: ${card.desc}</p>
-                        <a href="${card.links}">Campaign Link</a>
-                        ${linkElements}
-                    </div>
-                    <div class="col-sm-4">
-                        <img class="company-logo" src="${card.logo}" alt="card image for ${card.name}"/>
-                    </div>
-            </div>`;
+    const cardDiv = `<div class="company-card card row">
+                        <div class="row">
+                            <div class="col-sm-8 d-flex flex-column justify-content-center">
+                                ${formatCompanyName(card.name)}
+                                <br>
+                                <h5 class="card-subtitle">${card.location}</h5>
+                            </div>
+                            <div class="col-sm-4">
+                                <img class="company-logo" src="${card.logo}" alt="card image for ${card.name}"/>
+                            </div>
+                        </div>      
+                        <div>              
+                            <br>
+                            ${itemHighlighter(card.investType)}
+
+                            ${formatInvestType(itemHighlighter(card.impactType), card.impactTypeLink)}
+                            <p class="card-text">${itemHighlighter(card.desc)}</p>
+                            ${linkElements}
+                        </div>                    
+                    </div>`;
     if (card.region === currentRegion || currentRegion === 'all') {
         return cardDiv;
     } else if (currentRegion === 'US' && states.includes(card.region)) {
@@ -37,6 +62,7 @@ const generateCompanyCard = card => {
     }
 };
 
+// Empty card container and repopulate it
 const parseCompanies = companies => {
     $cardContainer.empty();
     companies.forEach( company => {
@@ -47,8 +73,10 @@ const parseCompanies = companies => {
 $(document).ready(() => {
     parseCompanies(companies);
 
-    $sortRegionBtn.on('click', function() {
+    $navRegionBtn.on('click', function() {
         currentRegion = $(this).attr('id');
         parseCompanies(companies);
+        $navRegionBtn.removeClass('shadow');
+        $(this).addClass('shadow');
     })
 });
