0f692a295c7e605f13c549b5456fa26b9f22f1a3
aundiks@gmail.com <andris@good-loop.com>
2019-09-27 09:11:42 +0100

    Portfolio: portfolio.js and template.html added. Previously not included for some reason

--- webroot/js/portfolio/portfolio.js
+++ webroot/js/portfolio/portfolio.js
@@ -8,7 +8,7 @@ let currentRegion = 'all';
 
 const generateCardLinks = links => {
     let renderedLinks = '';
-    links.forEach( link => {
+    links.forEach(link => {
         const linkKey = Object.keys(link)[0];
         renderedLinks += `<a class="company-link" href="${link[linkKey]}">${linkKey}</a>`
     });
@@ -24,56 +24,51 @@ const formatCompanyName = name => {
 const formatInvestType = (type, link) => {
     const splitString = type.split('(');
     return link ? `<p>${splitString[0]}<a href="${link}">(${splitString[1]}</a></p>`
-                : `<p>${type}</p>`;
+        : `<p>${type}</p>`;
 };
 
 const itemHighlighter = item => {
     const itemArray = item.split(':');
     return `<p><span style="font-weight: bold">${itemArray[0]}</span>: ${itemArray[1]}</p>`
 };
+{/* <img class="img-fluid w-100" src=${card.logo} alt=""></img> */}
+
+const getRegionCategory = card => {
+    if (card.region === 'UK') { return 'UK' }
+    else if (states.includes(card.region)) { return 'US' }
+    else { return 'WORLD' }
+}
 
 // Generate html card 'component' and return it, as long as it belong to currentRegion
 const generateCompanyCard = card => {
     const linkElements = generateCardLinks(card.links);
-    const cardDiv = `<div class="company-card card row">
-                        <div class="row">
-                            <div class="col-sm-8 d-flex flex-column justify-content-center">
-                                ${formatCompanyName(card.name)}
-                                <br>
-                                <h5 class="card-subtitle">${card.location}</h5>
-                            </div>
-                            <div class="col-sm-4">
-                                <img class="company-logo" src="${card.logo}" alt="card image for ${card.name}"/>
-                            </div>
-                        </div>      
-                        <div>              
-                            <br>
-                            ${itemHighlighter(card.investType)}
-
-                            ${formatInvestType(itemHighlighter(card.impactType), card.impactTypeLink)}
-                            <p class="card-text">${itemHighlighter(card.desc)}</p>
-                            ${linkElements}
-                        </div>                    
-                    </div>`;
-    if (card.region === currentRegion || currentRegion === 'all') {
-        return cardDiv;
-    } else if (currentRegion === 'US' && states.includes(card.region)) {
-        return cardDiv;
-    }
+    const regionCategory = getRegionCategory(card);
+    return (
+        `<div class="grid-item ${regionCategory}">
+        <div class="portfolio-item">
+            <div class="img-fluid w-100">
+            <img src="${card.logo}" class="card-logo" alt="company logo" />
+            </div>
+            <h3>${card.name}</h3>
+            <h4>${card.location}, ${card.region}</h4>
+        </div>
+    </div>`
+    );
 };
 
 // Empty card container and repopulate it
 const parseCompanies = companies => {
     $cardContainer.empty();
-    companies.forEach( company => {
-        $cardContainer.append(generateCompanyCard(company));
+    companies.forEach(company => {
+        $('.popup-gallery').append(generateCompanyCard(company));
     });
 };
 
 $(document).ready(() => {
     parseCompanies(companies);
+    $('.popup-gallery').append(generateCompanyCard(companies[0]));
 
-    $navRegionBtn.on('click', function() {
+    $navRegionBtn.on('click', function () {
         currentRegion = $(this).attr('id');
         parseCompanies(companies);
         $navRegionBtn.removeClass('shadow');
